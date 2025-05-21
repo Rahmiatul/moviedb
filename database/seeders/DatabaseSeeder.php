b192f1f9e9b791f47bb24ed072101adba3998fc8
Rahmiatul <rahmiatulhusna1109@gmail.com>
2025-05-21 16:14:43 +0700

    login

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,28 +1,53 @@
 <?php
 
+
+
 namespace Database\Seeders;
 
+
+
+use App\Models\Movie;
+
 use App\Models\User;
-use App\Models\Movie;  // Pastikan ini diimpor
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
 use Illuminate\Database\Seeder;
 
+
+
 class DatabaseSeeder extends Seeder
+
 {
+
     /**
+
      * Seed the application's database.
+
      */
+
     public function run(): void
+
     {
-        // Memanggil CategorySeeder
-        $this->call(CategorySeeder::class);
 
-        // Membuat 50 data Movie menggunakan factory
-        Movie::factory(50)->create();
+         User::factory(3)->create();
 
-        // Jika Anda ingin menambahkan user secara manual
-        // User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-    }
+
+
+              //  $this->call(CategorySeeder::class);
+
+             //   Movie::factory(50)->create();
+
+
+
+    //     User::factory()->create([
+
+    //         'name' => 'Test User',
+
+    //         'email' => 'test@example.com',
+
+    //     ]);
+
+     }
+
 }
